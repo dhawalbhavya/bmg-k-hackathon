fd14e0ebc60053680eb1ddf4a7ae1cebf0dd7405
Matt <meanslow@virginmedia.com>
2022-05-25 16:54:18 +0100

    crappy api working

---
+++ api/README.md
@@ -0,0 +1,11 @@
+# Amazing string reverser API
+it reverses strings. it can even return old reversed strings. amazing!!!
+
+## How to run
+```
+pip install -r requirements.txt
+python3 -m uvicorn main:app --reload
+```
+
+## Swagger
+Once app is running, navigate to http://127.0.0.1:8000/docs
---
+++ api/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import Optional
+
+
+class Message(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    message: str
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+engine = create_engine(sqlite_url, echo=True)
+
+
+app = FastAPI()
+SQLModel.metadata.create_all(engine)
+session = Session(engine)
+
+
+@app.get("/{message}")
+async def reverse_message(message: str):
+    reversed = message [::-1]
+    message_to_store = Message(message=reversed)
+    session.add(message_to_store)
+    session.commit()
+    return reversed
+
+
+@app.get("/message/{id}")
+async def get_message(id: int):
+    with Session(engine) as session:
+        statement = select(Message).where(Message.id == id)
+        results = session.exec(statement)
+        for result in results:
+            return result
+        return "No matches found!"
