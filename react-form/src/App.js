3810e2c325d5d1242d68661b898596259b67f3e7
nbs-tom-ramsden <tom.ramsden@nationwide.co.uk>
2022-05-26 13:49:21 +0100

    changes

--- react-form/src/App.js
+++ react-form/src/App.js
@@ -3,15 +3,17 @@ import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
-const API_BASE_URL = (message) => `http://localhost:9000/${message}`;
+const API_BASE_URL = `http://localhost:9000`;
+const API_MESSAGE = (message) => `${API_BASE_URL}/${message}`;
+const API_ID = (id) => `${API_BASE_URL}/message/${id}`;
 
 const [message, setMessage] = useState();
 const [ID, setID] = useState();
 
 function App() {
   const handleSubmit = () => {
-    const MESSAGE = (message !== "" ? message : ID)
-    axios.get(API_BASE_URL(MESSAGE)).then(res=>{alert(`response ${res.data}`)})
+    const URL = (message !== "" ? API_MESSAGE(message) : API_ID(ID))
+    axios.get(URL).then(res=>{alert(`response ${res.data}`)})
   }
 
   const handleMessageChange = (event) => {
